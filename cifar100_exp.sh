dfc3018add1bc79b08906f6591ec6688c2d68651
Your Name <you@example.com>
2025-12-16 11:24:14 +0100

    optional main

--- cifar100_exp.sh
+++ cifar100_exp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#PBS -N CA_4t1
-#PBS -o CA_4t1.txt
+#PBS -N CA_4t2
+#PBS -o CA_4t2.txt
 #PBS -q gpu
-#PBS -e CA_4t1.txt
+#PBS -e CA_4t2.txt
 #PBS -k oe
 #PBS -m e
 #PBS -M michele.russo03@leonardo.com
@@ -41,7 +41,7 @@ fi
 if [ "$train_model" -eq 1 ]; then
   for seed in $(seq 0 $((n_seed-1)))
   do
-      python /davinci-1/home/micherusso/PycharmProjects/Continual/main.py --run_name $run_name \
+      python /davinci-1/home/micherusso/PycharmProjects/Continual/main2.py --run_name $run_name \
               --dataset "CIFAR100" \
               --cuda 0 \
               --seed $seed \
